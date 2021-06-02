29047beeade8f9e5da65d807e2f9842a21d49681
AlexanderMetselaar <80575076+AlexanderMetselaar@users.noreply.github.com>
2021-06-02 21:51:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello WincAcademy");
+let name="Alexander";
+console.log(name);
+// Je kan geen gereserveerde keywords gebruiken (zoals false, return, if, whille)
+let som = 4 + 4;
+console.log(som);
+let a = 12+12;
+let b = 18*5;
+let c = 15-16;
+let d = 8 % 5
+console.log(a,b,c,d);
